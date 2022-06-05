b7f6ef3f2fe59f386130b03c7bd501eb03bf7508
Hadi Jamal Ahmad <hadi.jamalahmad@gmail.com>
2022-06-05 18:09:41 +0200

    second commit

--- TP1/MyScrypt.py
+++ TP1/MyScrypt.py
@@ -1,16 +1,9 @@
-# 
 import requests
-
-longitude = input('Enter your longitude :')
-latitude = input('Enter your latitude :')
-
-
-
-import requests
+import os
 import json
 api_key = "e9129c74b39d7c39f4009a8cc1d371d5"
-lat = str(latitude)
-lon = str(longitude)
+lat = os.environ['LAT']
+lon = os.environ['LONG']
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
 response = requests.get(url)
 data = json.loads(response.text)
