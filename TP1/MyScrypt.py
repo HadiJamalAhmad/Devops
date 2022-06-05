16e48d78edfa0ca73efabc366d08ffef4ba34f5e
Hadi Jamal Ahmad <hadi.jamalahmad@gmail.com>
2022-06-05 21:54:16 +0200

    third commit

--- TP1/MyScrypt.py
+++ TP1/MyScrypt.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import json
-api_key = "e9129c74b39d7c39f4009a8cc1d371d5"
+api_key = os.environ['API_KEY']
 lat = os.environ['LAT']
 lon = os.environ['LONG']
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
